140d729fe9a35437cb3ecac6237e15b17c14a396
Steve Seifert <sseifert@redhotchilibots.com>
2024-01-09 12:03:25 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import frc.robot.Constants.OIConstants;
+import frc.robot.autos.Autos;
+import frc.robot.subsystems.Chassis;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+/*
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+	// The robot's subsystems
+	private final Chassis chassis = new Chassis();
+	private final Autos auton = new Autos(chassis);
+
+	// The driver's controller
+	XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
+
+	/**
+	 * The container for the robot. Contains subsystems, OI devices, and commands.
+	 */
+	public RobotContainer() {
+		// Configure the button bindings
+		configureButtonBindings();
+
+		// Configure default commands
+		chassis.setDefaultCommand(
+				// The left stick controls translation of the robot.
+				// Turning is controlled by the X axis of the right stick.
+				new RunCommand(
+						() -> chassis.drive(
+								-MathUtil.applyDeadband(m_driverController.getLeftY() / 2, OIConstants.kDriveDeadband),
+								-MathUtil.applyDeadband(m_driverController.getLeftX() / 2, OIConstants.kDriveDeadband),
+								-MathUtil.applyDeadband(m_driverController.getRightX() / 2, OIConstants.kDriveDeadband),
+								true, true),
+						chassis));
+	}
+
+	/**
+	 * Use this method to define your button->command mappings. Buttons can be
+	 * created by
+	 * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
+	 * subclasses ({@link
+	 * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
+	 * passing it to a
+	 * {@link JoystickButton}.
+	 */
+	private void configureButtonBindings() {
+		new JoystickButton(m_driverController, Button.kX.value)
+				.whileTrue(new RunCommand(
+						() -> chassis.setX(),
+						chassis));
+	}
+
+	/**
+	 * Use this to pass the autonomous command to the main {@link Robot} class.
+	 *
+	 * @return the command to run in autonomous
+	 */
+	public Command getAutonomousCommand() {
+		return auton.getChooser().getSelected();
+	}
+}
